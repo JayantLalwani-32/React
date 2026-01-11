7ab7b4f8bcd12aef3f6d975f1faf71f4c09819a0
Jayant Lalwani <lalwanij32@gmail.com>
2026-01-12 00:21:10 +0530

    counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(10)
+  // let counter = 10;
+  const addValue = () => {
+    // counter++;
+    setCounter(counter + 1)
+    console.log(counter)
+  }
+  const decreaseValue = () => {
+    setCounter(counter--)
+    console.log(counter)
+  }
+
+  return (
+    <>
+      <h1>Heading</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Addvalue {counter}</button>
+      <br></br>
+      <button onClick={decreaseValue}>Decrease value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
